9c82591352dff8a0e7b28d926e7b271b59e0aef4
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-09-30 12:31:55 +0530

    Create functionsbyshortcode.php

---
+++ template/functionsbyshortcode.php
@@ -0,0 +1,11 @@
+<?php
+// Shortcode: [remote_template file="template-video-grid.php"]
+add_shortcode('remote_template', function($atts) {
+    $atts = shortcode_atts([
+        'file' => '',
+    ], $atts);
+
+    ob_start();
+    load_remote_template($atts['file']);
+    return ob_get_clean();
+});
