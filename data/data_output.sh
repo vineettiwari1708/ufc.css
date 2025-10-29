8d3bbe722e28045edaf9c28d32224b7b21da8da0
Vineet Tiwari <166735997+vineettiwari1708@users.noreply.github.com>
2025-10-29 11:54:35 +0530

    Create data_output.sh

---
+++ data/data_output.sh
@@ -0,0 +1,73 @@
+Array
+(
+    [0] => Array
+        (
+            [ID] => 123
+            [title] => 3BHK Luxury Apartment
+            [permalink] => http://your-site.com/property/3bhk-luxury-apartment
+            [price] => 1,50,00,000
+            [location] => Array
+                (
+                    [0] => Delhi
+                )
+            [area] => 1200 sqft
+            [bhk] => 3
+            [bedrooms] => 3
+            [bathrooms] => 3
+            [balconies] => 2
+            [floor] => 5
+            [furnishing] => Fully Furnished
+            [facing] => North
+            [maintenance] => 5000
+            [ownership] => Freehold
+            [rera_registered] => Yes
+            [construction_status] => Ready to Move
+            [age_of_property] => 2 years
+            [discount] => 5%
+            [latitude] => 28.6139
+            [longitude] => 77.2090
+            [neighbourhood] => Central Delhi
+            [full_address] => Connaught Place, Delhi, India
+            [landmark] => Near Metro Station
+            [pincode] => 110001
+            [power_backup] => Yes
+            [water_supply] => 24x7
+            [security] => CCTV Surveillance
+            [pet_friendly] => Yes
+            [view] => Park Facing
+            [internet] => High-Speed Internet
+            [sewage] => Proper sewage system
+            [gallery] => Array of image URLs
+            [video] => http://some-video-url.com
+            [image] => http://your-site.com/wp-content/uploads/property-thumbnail.jpg
+            [location_terms] => Array
+                (
+                    [0] => Delhi
+                )
+            [type_terms] => Array
+                (
+                    [0] => Apartment
+                )
+            [status_terms] => Array
+                (
+                    [0] => Available
+                )
+            [amenities_terms] => Array
+                (
+                    [0] => Gym
+                    [1] => Swimming Pool
+                )
+            [interior_terms] => Array
+                (
+                    [0] => Modular Kitchen
+                )
+            [smart_home_terms] => Array
+                (
+                    [0] => Yes
+                )
+            [loan_terms] => Array
+                (
+                    [0] => Available
+                )
+        )
+)
